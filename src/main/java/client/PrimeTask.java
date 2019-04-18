554b4324e807f3cad1a9c44da1e83555cf5d92e7
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-04-18 17:20:26 +0200

    expanded primeTask and tested it

--- src/main/java/client/PrimeTask.java
+++ src/main/java/client/PrimeTask.java
@@ -1,12 +1,34 @@
 package client;
 
+import java.util.ArrayList;
+
 public class PrimeTask {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PrimeTask task = new PrimeTask();
+        System.out.println(task.isPrime(10000019));
     }
 
-    public int firstMethod() {
-        return 18;
+    public ArrayList<Long> primesInRange(long from, long to) {
+        ArrayList<Long> list = new ArrayList<>();
+        for (long i = from; i <= to; i++) {
+            if (isPrime(i)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public boolean isPrime(long number) {
+        if (number < 3) {
+            return false;
+        }
+        for (long i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/client/PrimeTaskTest.java
+++ src/test/java/client/PrimeTaskTest.java
@@ -1,14 +1,36 @@
 package client;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class PrimeTaskTest {
 
+    private static PrimeTask task;
+
+    @BeforeClass
+    public static void setUp() {
+        task = new PrimeTask();
+    }
+
     @Test
-    public void firstMethod() {
-        PrimeTask task = new PrimeTask();
-        assertEquals(18, task.firstMethod());
+    public void isPrime() {
+        assertTrue(task.isPrime(5));
+        assertFalse(task.isPrime(4));
+        assertFalse(task.isPrime(1));
+    }
+
+    @Test
+    public void primesInRange() {
+        ArrayList<Long> list = new ArrayList<>();
+        assertEquals(list, task.primesInRange(-3, 1));
+
+        list.add(3L);
+        list.add(5L);
+        list.add(7L);
+        assertEquals(list, task.primesInRange(0, 8));
     }
 }
