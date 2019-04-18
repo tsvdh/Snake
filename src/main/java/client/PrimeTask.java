e0dd854edf461f2b6bcec9ba71a9570a440514fe
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-04-18 11:39:36 +0200

    first commit, with test

---
+++ src/main/java/client/PrimeTask.java
@@ -0,0 +1,12 @@
+package client;
+
+public class PrimeTask {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int firstMethod() {
+        return 18;
+    }
+}
---
+++ src/test/java/client/PrimeTaskTest.java
@@ -0,0 +1,13 @@
+package client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrimeTaskTest {
+
+    @Test
+    public void firstMethod() {
+        assertEquals(18, PrimeTask.firstMethod());
+    }
+}
