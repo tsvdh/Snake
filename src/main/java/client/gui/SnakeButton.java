20fcb4a90c66c43fdf13f7c96ca3ad3b28aabb09
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-04-25 11:39:01 +0200

    made custum button class

---
+++ src/main/java/client/gui/SnakeButton.java
@@ -0,0 +1,41 @@
+package client.gui;
+
+import javafx.scene.control.Button;
+
+class SnakeButton extends Button {
+
+    String STATUS;
+
+    SnakeButton() {
+        super();
+        this.setMinSize(30, 30);
+        this.setEmpty();
+    }
+
+    void setEmpty() {
+        this.STATUS = "empty";
+        this.setStyle("-fx-background-color: lightgrey;"
+                + "-fx-border-style: solid;"
+                + "-fx-border-width: 1px;"
+                + "-fx-border-color: black;"
+                + "-fx-border-radius: 0px");
+    }
+
+    void setSnake() {
+        this.STATUS = "snake";
+        this.setStyle("-fx-background-color: green;"
+                + "-fx-border-style: solid;"
+                + "-fx-border-width: 1px;"
+                + "-fx-border-color: black;"
+                + "-fx-border-radius: 0px");
+    }
+
+    void setApple() {
+        this.STATUS = "apple";
+        this.setStyle("-fx-background-color: red;"
+                + "-fx-border-style: solid;"
+                + "-fx-border-width: 1px;"
+                + "-fx-border-color: black;"
+                + "-fx-border-radius: 0px");
+    }
+}
