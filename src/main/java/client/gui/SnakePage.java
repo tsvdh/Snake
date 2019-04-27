bddad434fa3d81e97159ad4bdfe9d05321d5f958
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-04-27 12:59:53 +0200

    added option to play again when pressing enter

--- src/main/java/client/gui/SnakePage.java
+++ src/main/java/client/gui/SnakePage.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -276,15 +277,21 @@ class SnakePage {
                                 quitButton);
         hBox.setSpacing(15);
         hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(20, 0, 0, 0));
 
-        Label label = new Label();
-        label.setText("Game over!");
-        label.setFont(new Font(25));
+        Label messageLabel = new Label();
+        messageLabel.setText("Game over!");
+        messageLabel.setFont(new Font(25));
+
+        Label optionLabel = new Label();
+        optionLabel.setText("Press enter to play again");
+        optionLabel.setFont(new Font(14));
 
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(label,
-                                hBox);
-        vBox.setSpacing(30);
+        vBox.getChildren().addAll(messageLabel,
+                                hBox,
+                                optionLabel);
+        vBox.setSpacing(20);
         vBox.setAlignment(Pos.CENTER);
         vBox.setPadding(new Insets(20));
 
@@ -293,7 +300,8 @@ class SnakePage {
         //Setting scene and stage
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setScene(new Scene(vBox));
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
 
         //Setting the button actions
         quitButton.setOnAction(event -> {
@@ -304,6 +312,12 @@ class SnakePage {
         againButton.setOnAction(event -> {
             stage.close();
             setUpGame();
+        });
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                againButton.fire();
+            }
         });
 
         stage.showAndWait();
