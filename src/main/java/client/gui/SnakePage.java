f4c8039fab80acb768905d0bc659748af85046d2
Tobias van den Hurk <t.s.vandenhurk@student.tudelft.nl>
2019-05-02 17:57:04 +0200

    apple spawned in random position again

--- src/main/java/client/gui/SnakePage.java
+++ src/main/java/client/gui/SnakePage.java
@@ -257,11 +257,11 @@ class SnakePage {
 
     private static void spawnApple() {
         Random randomGenerator = new Random();
-        //int x = randomGenerator.nextInt(20);
+        int x = randomGenerator.nextInt(20);
         int y = randomGenerator.nextInt(20);
 
-        if (gridArray[y][snakeList.getFirst().X].STATUS.equals("empty")) {
-            gridArray[y][snakeList.getFirst().X].setApple();
+        if (gridArray[y][x].STATUS.equals("empty")) {
+            gridArray[y][x].setApple();
         } else {
             spawnApple();
         }
